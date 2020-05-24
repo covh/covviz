88e765af177cb16c24223197784e4f1938f56d29
Andreas Krueger <covid19hacks@protonmail.com>
2020-05-24 18:04:52 +0200

    code and site can be generated on different machines -->  git pull first

--- scripts/downloadAndUpdate.sh
+++ scripts/downloadAndUpdate.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-git pull
+LOGFILE="logs/$(date +%Y%m%d-%H%M)_downloadAndUpdate.log"
 
+# make sure to work on the newest code
+git pull | tee -a $LOGFILE
+
+# merge with newer site data possibly generated on different machine 
+cd ../cov19de
+echo $(pwd) | tee -a ../covviz/$LOGFILE
+git pull | tee -a ../covviz/$LOGFILE
+
+# come back to code repo
+cd ../covviz
+echo $(pwd) | tee -a $LOGFILE
+
+# python dependencies, enter and log source folder
 source  ./py3science/bin/activate
 cd src
+echo $(pwd) | tee -a ../$LOGFILE
 
-python downloadAndUpdate.py | tee -a "../logs/$(date +%Y%m%d-%H%M)_downloadAndUpdate.log"
+# the whole shebang
+python downloadAndUpdate.py | tee -a ../$LOGFILE
 
 
