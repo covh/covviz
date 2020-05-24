281a8d3cd27cfe5c543e2cbe8149a14d8b82c019
Andreas Krueger <covid19hacks@protonmail.com>
2020-05-24 22:48:27 +0200

    let scripts exit when any step fails

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#!/bin/bash
+
+# exit when any command fails
+set -e
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo; echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
+
 LOGFILE="logs/$(date +%Y%m%d-%H%M)_initialize.log"
 
 # make sure to work on the newest code
