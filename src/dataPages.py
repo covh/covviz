4ca0884503499a3999892e34824b5cc7bfd4ea3f
Andreas Krueger <covid19hacks@protonmail.com>
2020-07-17 08:17:23 +0200

    new hotspots

--- src/dataPages.py
+++ src/dataPages.py
@@ -510,10 +510,11 @@ def generate_hotspot_files():
     distances = districtDistances.load_distances()
     print ("50 km, relative threshold:")
     for AGS in (5558, 16072, 9163, 16076, 9473, 9263, 9278, 8231, 4011, 5382, 9362, 9478, 5370, 3459, 9463, 9376, 9475, 5554, 8231,
-                4012, 3352, 16052, 9473, 7315, 3159, 9771, 5754, 3361, 15003, 5570, 3103, 6632, 3458, 3401, 5170, 5111, 5915, 5112, 9188, 9279, 7334):
+                4012, 3352, 16052, 9473, 7315, 3159, 9771, 5754, 3361, 15003, 5570, 3103, 6632, 3458, 3401, 5170, 5111, 5915, 5112,
+                9188, 9279, 7334, 9173,5366,5158,7312):
         neighbour_districts_table_page(AGS=AGS, distances=distances, km=50, bnn=bnn)
     print ("\n50 km, absolute threshold, and not already among relative threshold:")
-    for AGS in (11000, 5112, 3241, 9162, 5913, 4011, 5315, 6412, ):
+    for AGS in (11000, 5112, 3241, 9162, 5913, 4011, 5315, 6412, 9761, 5958):
         neighbour_districts_table_page(AGS=AGS, distances=distances, km=50, bnn=bnn)
     
     print ("\n100 km:")
