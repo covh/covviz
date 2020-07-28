219cd5f8b2b6c1528b337546becef4dadbc5f4fc
Andreas Krueger <covid19hacks@protonmail.com>
2020-07-28 21:59:02 +0200

    repeat script now with several download moments during night and day

--- scripts/repeat.sh
+++ scripts/repeat.sh
@@ -2,24 +2,56 @@
 
 ####   nohup ./repeat.sh >> nohup.out 2>&1 &
 
+echo 
+echo "###############################################################"
+echo "# started new"
+echo "###############################################################"
+date
 echo
-echo until the new CSV is there at approx 2am ...
-echo sleep 18000
-sleep 18000
+echo sleep until the new CSV is there at approx 2am ...
+echo sleep  14400
+sleep 14400
+echo slept.
+
 echo
 echo now loop:
 
 while true
 do
-  echo next round, at
+  echo doing next round, at
   date
   echo
+  cd /root/covh/covviz; scripts/downloadAndUpdate.sh
 
+  echo 
+  echo now sleep a bit less than 2 hours and just do it again
+  echo sleep 6700
+  sleep 6700
+  echo slept.
+
+
+  echo repeated night round, at
+  date
+  echo
   cd /root/covh/covviz; scripts/downloadAndUpdate.sh
 
   echo
-  echo now sleep 86000
-  sleep 86000
+  echo now sleep a bit less than 12 hours and catch the afternoon data
+  echo sleep 42700
+  sleep 42700
   echo slept.
   echo
+
+  echo afternoon round, at
+  date
+  echo
+  cd /root/covh/covviz; scripts/downloadAndUpdate.sh
+
+  echo
+  echo now sleep a bit less than 10 hours and do the next night
+  echo sleep 35500
+  sleep 35500
+  echo slept.
+  echo
+
 done
