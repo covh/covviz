7cc06841c3bf43348c8d613f01cdb2dda881ccdd
jalsti <57985357+jalsti@users.noreply.github.com>
2020-09-20 20:16:47 +0200

    plots with grids; also minor tick count reduced at x-axis for better visibility; skew of expexctation day triangle fixed

--- src/dataPlotting.py
+++ src/dataPlotting.py
@@ -28,14 +28,19 @@ def plot_timeseries(datacolumns, dates, daily, cumulative, title, filename, ifSh
 
     fig, ax = plt.subplots(figsize=(10, 6)) #, constrained_layout=True)
     # plt.tight_layout()
+    plt.grid(True, which='major', axis='x', ls='-', alpha=0.9) # if not set here above, major ticks of x axis won't be visible
+    plt.grid(True, which='minor', axis='x', ls='--', alpha=0.7) # if not set here above, minor ticks of x axis won't be visible
+    plt.grid(True, which='major', axis='y', ls='-', alpha=0.9) # if not set here above, ticks of left side y axis won't be visible
+    plt.grid(True, which='minor', axis='y', ls='--', alpha=0.7)
 
     # x axis
     ax.xaxis_date()
-    ax.xaxis.set_minor_locator(matplotlib.dates.DayLocator())
     fig.autofmt_xdate(rotation=60)
+    ax.xaxis.set_minor_locator(matplotlib.dates.DayLocator(bymonthday=range(1, 30, 5)))
+    ax.yaxis.set_minor_locator(matplotlib.ticker.MultipleLocator(2))
 
     # plot data
-    lns1 = ax.plot(dates, daily, label="daily cases (weekend-flawed), 2 weeks: red", color='lightgray')
+    lns1 = ax.plot(dates, daily, label="daily cases (weekend-flawed), 2 weeks: red", color='#AAAAAA')
     lns1_2 = ax.plot(dates[-14:], daily[-14:], label="daily cases, last 14 days dark gray", color='red')
     # print (len(dates[-14:]))
     
@@ -62,7 +67,7 @@ def plot_timeseries(datacolumns, dates, daily, cumulative, title, filename, ifSh
     # lns4_2 = plt.plot(dates[int(round(center))], max(signal), marker="v", color='green', markersize=15)
     # lns4_2 = plt.plot(dates[int(round(center))], 0, marker="v", color='green', markersize=15)
     # lns4_2 = plt.plot(dates[int(round(center))], [max(daily[1:])/20], marker="^", color='green', markersize=30)
-    lns4_2 = plt.plot(dates[int(round(center))], [max(daily[1:])/19], marker="v", color='green', markersize=17)
+    lns4_2 = plt.plot(dates[int(round(center))], [max(daily[1:])/19], marker="v", color='green', markersize=16) # if markersize is an odd number, the triangle will be skewed
 
 
     # plot 2nd axis and cumulative data
@@ -83,14 +88,17 @@ def plot_timeseries(datacolumns, dates, daily, cumulative, title, filename, ifSh
     labs = [l.get_label() for l in lines]
 
     text = "source data @RiskLayer up to " + ("%s"%max(dates))[:10]
-    text += "\nplot @DrAndreasKruger " + ("%s" % datetime.datetime.now())[:16]
+    text += "\nplot @DrAndreasKruger (+contrib.) " + ("%s" % datetime.datetime.now())[:16]
     text += "\ndaily: (GREEN) 'expectation day' = "+center_date
 
     plt.legend(lines, labs, loc='upper left', facecolor="#fafafa", framealpha=0.8, 
                title=text, prop={'size': 8}, title_fontsize = 8)
 
     plt.title(title)
-    
+
+    ax.xaxis.set_major_locator(matplotlib.dates.DayLocator(bymonthday=range(1, 32, 31))) # if placing this setting above all other, major ticks won't appear
+    ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%Y-%m")) # its date formatter must be set, if setting major locator
+
     if filename:
         fig.savefig(os.path.join(dataFiles.PICS_PATH, filename),  bbox_inches='tight')
     
