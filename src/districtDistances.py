d931c9291ab2499f6a3b62d3e39741bba3dff08d
jalsti <57985357+jalsti@users.noreply.github.com>
2020-09-22 09:04:59 +0200

    changed field name for regional code to current one

--- src/districtDistances.py
+++ src/districtDistances.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-@summary: Opendatasoft database into pairwise distances of all districts of Germany 
+@summary: Opendatasoft database into pairwise distances of all districts of Germany
 
 @version: v03.5.2 (9/Sept/2020)
 @since:   27/April/2020
@@ -40,14 +40,14 @@ pip3 install -U pip wheel
 pip install -r requirements.txt
 
 cd src
-python3 districtDistances.py 
+python3 districtDistances.py
 
 # in first run, change to GENERATE_NEW = True
 """
 
 ####################
 
-GENERATE_NEW = False # If you set this to True, the pairwise calculations will need time. Switch back to False afterwards. 
+GENERATE_NEW = False # If you set this to True, the pairwise calculations will need time. Switch back to False afterwards.
 
 ####################
 
@@ -62,6 +62,7 @@ import wget
 import dataMangling, dataFiles
 from dataFiles import DATA_PATH, OPENDATASOFT_URL01, OPENDATASOFT_URL02, OPENDATASOFT_PATH, DISTANCES_PATH
 
+CSV_FIELD_REGIONAL_CODE="Regional code"
 
 def download_kreise_locations(url1=OPENDATASOFT_URL01, url2=OPENDATASOFT_URL02, out=OPENDATASOFT_PATH):
     print ("Downloading large table with Kreise locations. For infos see")
@@ -76,13 +77,13 @@ def download_kreise_locations(url1=OPENDATASOFT_URL01, url2=OPENDATASOFT_URL02, 
     return filename
 
 def repair_kreise_locations(LKG):
-    # Goettingen old=3152 new 3159 
-    i=LKG[LKG["Cca 2"]==3152].index.tolist()
+    # Goettingen old=3152 new 3159
+    i=LKG[LKG[CSV_FIELD_REGIONAL_CODE]==3152].index.tolist()
     if i:
-        LKG.at[i, "Cca 2"] = 3159
-        
+        LKG.at[i, CSV_FIELD_REGIONAL_CODE] = 3159
+
     # Osterode old=3156, now part of 3159
-    i=LKG[LKG["Cca 2"]==3156].index.tolist()
+    i=LKG[LKG[CSV_FIELD_REGIONAL_CODE]==3156].index.tolist()
     if i:
         LKG.drop(index=i, inplace=True)
     print ("Done repairing data (Osterode, Goettingen) --> Goettingen AGS 3159.")
@@ -93,17 +94,17 @@ def load_kreise_locations(filename = OPENDATASOFT_PATH):
     # LKG=pandas.read_csv(io.StringIO(s.decode('utf-8')), sep=';') # error_bad_lines=False)
     LKG=pandas.read_csv(filename, sep=';')
     # immediately drop non-Kreis row(s)
-    LKG.dropna(subset=['Cca 2'],inplace=True)
+    LKG.dropna(subset=[CSV_FIELD_REGIONAL_CODE],inplace=True)
     # turn AGS into integer:
-    LKG["Cca 2"]=LKG["Cca 2"].astype(int)
+    LKG[CSV_FIELD_REGIONAL_CODE]=LKG[CSV_FIELD_REGIONAL_CODE].astype(int)
     print ("Done downloading.")
     LKG = repair_kreise_locations(LKG)
-    
+
     return LKG
 
 def accelerate_lookup(LKG):
     # accelerate lookup
-    AGS_to_geopoint=dict(LKG[["Cca 2","Geo Point"]].set_index("Cca 2").to_dict('series')["Geo Point"])
+    AGS_to_geopoint=dict(LKG[[CSV_FIELD_REGIONAL_CODE,"Geo Point"]].set_index(CSV_FIELD_REGIONAL_CODE).to_dict('series')["Geo Point"])
     print ("Done generating lookup dict")
     return AGS_to_geopoint
 
@@ -136,13 +137,13 @@ def make_distances_table(AGS_to_geopoint, all_AGS, filename=DISTANCES_PATH):
 
     distances["AGS1"]=distances["AGS1"].astype(int)
     distances["AGS2"]=distances["AGS2"].astype(int)
-        
+
     print ("Distances are distributed like this:")
     print(distances["km"].describe())
-    
+
     distances.sort_values("km", inplace=True)
     print("Done sorting.")
-    
+
     distances.to_csv(filename, index=False)
     print ("Saved to", filename)
     return distances, filename
@@ -156,11 +157,11 @@ def number_of_pairs_max_dist(distances, km):
 
 def nearby(distances, AGS, km):
     return distances[(distances.km<=km) & (distances.AGS1==AGS)]
-    
+
 def kreis_link(bnn, AGS):
     nameAndType = dataMangling.AGS_to_name_and_type(bnn, AGS)
     name_BL, inf_BL, pop_BL = dataMangling.AGS_to_Bundesland(bnn, AGS)
-    AGS_5digits = ("00000%s" % AGS) [-5:] 
+    AGS_5digits = ("00000%s" % AGS) [-5:]
     filename = "%s.html#AGS%s" % (name_BL, AGS_5digits)
     link='<a id="%s" href="%s">%s</a>' % (nameAndType, filename, nameAndType) # also give it an id, so sorting alphabetically works even though the filename starts with bundesland
     return filename, nameAndType, link
@@ -174,17 +175,17 @@ def kreis_nearby_links(bnn, distances, AGS, km=50):
         # print (filename, nameAndType)
         linklist.append(link)
     return ", ".join(linklist)
-    
-    
+
+
 def compare_risklayer_with_opendatasoft(bnn):
     filename = dataFiles.OPENDATASOFT_PATH
     LKG = load_kreise_locations(filename)
-    ODS = set(LKG["Cca 2"].tolist())
+    ODS = set(LKG[CSV_FIELD_REGIONAL_CODE].tolist())
     ts, bnn = dataFiles.data(withSynthetic=True)
     RSL=set(bnn["AGS"].values.tolist())
-    
+
     diff1 = list(ODS-RSL)
-    names1=([LKG["Name 2"][LKG["Cca 2"]==AGS].tolist()[0] for AGS in diff1])
+    names1=([LKG["Name 2"][LKG[CSV_FIELD_REGIONAL_CODE]==AGS].tolist()[0] for AGS in diff1])
     print ("ODS-RSL: %s = %s"% (diff1, names1))
     diff2 = list(RSL-ODS)
     names2=([bnn["GEN"][bnn["AGS"]==AGS].tolist()[0] for AGS in diff2])
@@ -192,34 +193,34 @@ def compare_risklayer_with_opendatasoft(bnn):
 
 
 def downloadFromOpendatasoft_and_generatePairwiseDistancesFile():
-    
+
     print ("\nWe want the neighbour districts. So first download the GPS midpoints and then calculate the pairwise distances, and sort them.\n")
-     
+
     # filename = OPENDATASOFT_PATH
     filename = download_kreise_locations()
 
     LKG = load_kreise_locations(filename)
-    
+
     AGS_to_geopoint = accelerate_lookup(LKG)
     print ("sample distance", geo_distance(AGS_to_geopoint, 1001, 1002))
 
-    all_AGS=LKG["Cca 2"].dropna().astype(int).tolist()
+    all_AGS=LKG[CSV_FIELD_REGIONAL_CODE].dropna().astype(int).tolist()
     # all_AGS=all_AGS[:40] # reduce number for dev'ing
     print("now pairwise distances for %d locations" % len(all_AGS))
     print()
-    
+
     distances, filename = make_distances_table(AGS_to_geopoint, all_AGS)
-    
+
     AGS1, km = all_AGS[0], 150
     print("testing with AGS=%d and km=%d:" % (AGS1, km))
     print (distances[(distances.km<km) & (distances.AGS1==AGS1)])
 
     return filename
-    
+
 
 if __name__ == '__main__':
-    
-    
+
+
     if GENERATE_NEW:
         downloadFromOpendatasoft_and_generatePairwiseDistancesFile()
 
@@ -227,7 +228,7 @@ if __name__ == '__main__':
     # print ("\ns**t that had been inconsistent data:")
     # compare_risklayer_with_opendatasoft(bnn)
     # print ("\nall good.")
-    
+
     try:
         distances = load_distances()
     except Exception as e:
@@ -242,7 +243,7 @@ if __name__ == '__main__':
     #print (kreis_link(bnn, 0))
     #print (kreis_link(bnn, 1001))
     # print (kreis_link(bnn, 3152))
-    
+
     # print (districtDistances.nearby(distances, 1001, 50))
     #AGS=1001
     #print( kreis_nearby_links(bnn, distances, AGS, km=50) )
